083dd23dd3ef5904429e0ed43f8f457ec45c7a8a
qqqq4u <danila16102003@rambler.ru>
2023-02-13 23:00:34 +0300

    some mOOOOre fixes

--- tests/set.js
+++ tests/set.js
@@ -30,10 +30,10 @@ QUnit.module('Тестируем функцию set', function () {
 			deep: null
 		};
 
-		assert.deepEqual(set({ foo: 'bar' }, '.foo', 'baz'), { foo: 'baz' });
+		assert.deepEqual(set({foo:'bar'}, '.foo', 'baz'), {foo:'baz'});
 		assert.deepEqual(set(object, '.deep.hested.field', 42), object2);
 
-		assert.deepEqual(set(object, '.deep.hested', { foo: 'bar' }), object3);
+		assert.deepEqual(set(object, '.deep.hested', {foo: 'bar'}), object3);
 		assert.deepEqual(set(object, '.deep', null), object4);
 	});
 
@@ -53,30 +53,30 @@ QUnit.module('Тестируем функцию set', function () {
 
 	QUnit.test('set работает правильно c массивами', function (assert) {
 		const object1 = {
-			foo: [1, 2, 3],
+			foo: [ 1, 2, 3 ],
 			bar: [
-				{ foobar: '42' }
+				{foobar: '42'}
 			]
 		};
 
 		const object2 = {
-			foo: [1, 2, 3],
+			foo: [ 1, 2, 3 ],
 			bar: [
-				{ foobar: '42' }
+				{foobar: '42'}
 			]
 		};
 
 		const new1 = {
-			foo: [42, 2, 3],
+			foo: [ 42, 2, 3 ],
 			bar: [
-				{ foobar: '42' }
+				{foobar: '42'}
 			]
 		};
 
 		const new2 = {
-			foo: [1, 2, 3],
+			foo: [ 1, 2, 3 ],
 			bar: [
-				{ foobar: 'baz' }
+				{foobar: 'baz'}
 			]
 		};
 
@@ -135,7 +135,7 @@ QUnit.module('Тестируем функцию set', function () {
 
 		assert.deepEqual(set(object, '.foo', [42, 24, 30]), object1);
 	});
-
+	
 	QUnit.test('set работает правильно при вставке сложной структуры в пустой массив', function (assert) {
 		const object = {
 			foo: []
@@ -154,5 +154,29 @@ QUnit.module('Тестируем функцию set', function () {
 			nested:
 				{ field: ["I", "love", "technopark!"] }
 		}), object1);
+	});
+	
+	QUnit.test('set выбрасывает ошибку при неправильном формате ввода', function (assert){
+		const object = {
+			foo: 'bar'
+		};
+	
+		const object1 = {
+			foo: 'baz'
+		};
+
+		assert.throws (()=>set(object,object,'baz'), new TypeError('Path need to be a string!'));
+		assert.throws (()=>set(object,NaN,'baz'),new TypeError('Path need to be a string!'));
+		assert.throws (()=>set(object,123243223,'baz'),new TypeError('Path need to be a string!'));
+		assert.throws (()=>set(object,null,'baz'),new TypeError('Path need to be a string!'));
+		assert.throws (()=>set(object,undefined,'baz'),new TypeError('Path need to be a string!'));
+
+		assert.throws (()=>set(undefined,'.foo','baz'),new TypeError('Wrong object type'));
+		assert.throws (()=>set(NaN,'.foo','baz'),new TypeError('Wrong object type'));
+		assert.throws (()=>set((value) => {return value},'.foo','baz'),new TypeError('Wrong object type'));
+
+		assert.throws (()=>set(object,'.foo',Math.sqrt(-1)),new TypeError('Wrong value type'));
+		assert.throws (()=>set(object,'.foo',undefined),new TypeError('Wrong value type'));
+		assert.throws (()=>set(object,'.foo',(value) => {return value}),new TypeError('Wrong value type'));
 	});
 });
